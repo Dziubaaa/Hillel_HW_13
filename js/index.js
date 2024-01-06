f49d18029f1f2c640c5ea4eeca61d67243b20b8a
Volodymyr Dziuba <vovadziuba93@gmail.com>
2024-01-06 17:59:53 +0200

    deleted html

--- js/index.js
+++ js/index.js
@@ -26,31 +26,3 @@ function prevImg() {
 }
 
 showImg(currentImgIndex);
-
-// const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
-// let currentImgIndex = 0;
-
-// function showImg(index) {
-//     const image = document.getElementById('currentImg');
-//     image.src = `./img/${images[index]}`;
-
-//     document.getElementById('prevBtn').style.display = (index === 0) ? 'none' : 'inline-block';
-
-//     document.getElementById('nextBtn').style.display = (index === images.length - 1) ? 'none' : 'inline-block';
-// }
-
-// function nextImg() {
-//     if (currentImgIndex < images.length - 1) {
-//         currentImgIndex++;
-//         showImg(currentImgIndex);
-//     }
-// }
-
-// function prevImg() {
-//     if(currentImgIndex > 0) {
-//         currentImgIndex--;
-//         showImg(currentImgIndex);
-//     }
-// }
-
-// showImg(currentImgIndex);
